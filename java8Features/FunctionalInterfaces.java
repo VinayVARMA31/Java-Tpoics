4a2a6c4c72a8ccedd84639ba039dccf47570d489
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-12-02 17:24:05 +0530

    created a custom interface

--- java8Features/FunctionalInterfaces.java
+++ java8Features/FunctionalInterfaces.java
@@ -1,9 +1,18 @@
 import java.util.function.UnaryOperator;
 
+interface Calculate{
+  
+    int cal(int n);
+}
 public class FunctionalInterfaces {
   public static void main(String[] args) {
     UnaryOperator<Integer> square = x-> x*x;
 
     System.out.println(square.apply(5));
+
+    Calculate multiply = x-> x*2;
+
+    System.out.println(multiply.cal(5));
+
   }
 }
