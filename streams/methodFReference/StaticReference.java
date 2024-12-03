b20818a76e8d04b1f8466993e819a6d7ecd801df
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-12-03 08:21:54 +0530

    Non-Static method reference

--- streams/methodFReference/StaticReference.java
+++ streams/methodFReference/StaticReference.java
@@ -5,11 +5,19 @@ public class StaticReference {
     public static Integer cal(int i){
         return i*i;
     }
+    public Integer doubleNum(int i){
+        return 2*i;
+    }
 }
  class Test {
   public static void main(String[] args) {
+    //Static method reference
     Function<Integer,Integer> square= StaticReference::cal;
     System.out.println(square.apply(5));
+    //Non-Static method reference 
+    StaticReference sr = new StaticReference();
+    Function<Integer,Integer> twoTimes = sr::doubleNum;
+    System.out.println(twoTimes.apply(5));
   }
     
 }
